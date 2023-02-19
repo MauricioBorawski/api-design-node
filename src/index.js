4149fd34d4afd77ff7fd6147f8568a8baf9cde22
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-02-18 22:12:24 -0300

    First lesson
    Create project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    console.log("Hello World")
+    res.end();
+  }
+});
+
+server.listen(3001, () => {
+  console.log("Server on http://localhost:3001");
+});
